57680f0297d0c14386712646898f07e6253547d1
AgustinLeguizamon <38507913+AgustinLeguizamon@users.noreply.github.com>
2019-10-03 11:38:59 -0300

    TP1

---
+++ asm-helpers.c
@@ -0,0 +1,12 @@
+#include "builders.h"
+#include "ant_engine.h" /*agregado*/
+
+void doPanic(char *s)
+{
+  panic(s);
+}
+
+void doAdjust(uint32_t *p, uint32_t v, uint32_t limit)
+{
+	adjust (p, v, limit);
+}
